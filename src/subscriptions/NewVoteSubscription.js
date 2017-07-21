17795c70256a5e778d47325d06b2cabdc98faf86
Nikolas <nikolas.burk@gmail.com>
2017-07-21 18:41:05 +0200

    Update NewVoteSubscription.js

--- src/subscriptions/NewVoteSubscription.js
+++ src/subscriptions/NewVoteSubscription.js
@@ -3,10 +3,14 @@ import {
   requestSubscription
 } from 'react-relay'
 import environment from '../Environment'
+import {ConnectionHandler} from 'react-relay'
+
 
 const newVoteSubscription = graphql`
   subscription NewVoteSubscription {
-    Vote {
+    Vote(filter: {
+      mutation_in: [CREATED]
+    }) {
       mutation
       node {
         id
@@ -24,13 +28,23 @@ const newVoteSubscription = graphql`
   }
 `
 
-export default (updater, onError) => {
+export default () => {
 
   const subscriptionConfig = {
     subscription: newVoteSubscription,
     variables: {},
-    updater,
-    onError
+    updater: proxyStore => {
+      const createVoteField = proxyStore.getRootField('Vote')
+      const newVote = createVoteField.getLinkedRecord('node')
+      const updatedLink = newVote.getLinkedRecord('link')
+      const linkId = updatedLink.getValue('id')
+      const newVotes = updatedLink.getLinkedRecord('_votesMeta')
+      const newVoteCount = newVotes.getValue('count')
+
+      const link = proxyStore.get(linkId)
+      link.getLinkedRecord('votes').setValue(newVoteCount, 'count')
+    },
+    onError: error => console.log(`An error occured:`, error)
   }
 
   requestSubscription(
